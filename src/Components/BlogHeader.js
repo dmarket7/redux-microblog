0b1f00e21347436f672d1d71bcf6b160ead6ffbd
Dario Mercado <dariomercado@darios-mbp.attlocal.net>
2019-11-09 18:02:18 -0800

    added color to BlogHeader component, modified margin

--- src/Components/BlogHeader.js
+++ src/Components/BlogHeader.js
@@ -1,22 +1,21 @@
 import React, { Component } from "react";
 import { Jumbotron, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import './BlogHeader.css';
 
 class BlogHeader extends Component {
   render() {
     return (
-      <div className="mt-5">
-        <Jumbotron>
-          <h1 className="display-3">Hello, world!</h1>
-          <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-          <hr className="my-2" />
-          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-          <p className="lead">
-            <Link to="/" className="mx-3"><Button color="info">Home</Button></Link>
-            <Link to="/new" className="mx-3"><Button color="primary">Add New Blog</Button></Link>
-          </p>
-        </Jumbotron>
-      </div>
+      <Jumbotron className="bg-warning">
+        <h1 className="display-3">Microblog!</h1>
+        <p className="lead">Click below to add a new blog post...</p>
+        <hr className="my-2" />
+        <p>We look forward to blogging with you.</p>
+        <p className="lead">
+          <Link to="/" className="mx-3"><Button color="info">Home</Button></Link>
+          <Link to="/new" className="mx-3"><Button color="primary">Add New Blog</Button></Link>
+        </p>
+      </Jumbotron>
     );
   }
 }
